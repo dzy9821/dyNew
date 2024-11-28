5952d14042e5140006dd47e9ec2c356dc38a04aa
Zhaoyuan <Zhaoyuan.Duan@campus.lmu.de>
2024-11-28 15:46:47 +0100

    day 3 wemedia channel

---
+++ heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/service/impl/WmMaterialServiceImpl.java
@@ -0,0 +1,107 @@
+package com.heima.wemedia.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.heima.file.service.FileStorageService;
+import com.heima.model.common.dtos.PageResponseResult;
+import com.heima.model.common.dtos.ResponseResult;
+import com.heima.model.common.enums.AppHttpCodeEnum;
+import com.heima.model.wemedia.dtos.WmMaterialDto;
+import com.heima.model.wemedia.pojos.WmMaterial;
+import com.heima.utils.thread.WmThreadLocalUtil;
+import com.heima.wemedia.mapper.WmMaterialMapper;
+import com.heima.wemedia.service.WmMaterialService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.UUID;
+
+
+@Slf4j
+@Transactional
+@Service
+public class WmMaterialServiceImpl extends ServiceImpl<WmMaterialMapper, WmMaterial> implements WmMaterialService {
+
+
+    private final FileStorageService fileStorageService;
+
+    public WmMaterialServiceImpl(FileStorageService fileStorageService) {
+        this.fileStorageService = fileStorageService;
+    }
+
+    /**
+     * 图片上传
+     *
+     * @param multipartFile
+     * @return
+     */
+    @Override
+    public ResponseResult uploadPicture(MultipartFile multipartFile) {
+
+        //检查参数
+        if (multipartFile == null || multipartFile.getSize() == 0) {
+            return ResponseResult.errorResult(AppHttpCodeEnum.PARAM_INVALID);
+        }
+        //上传图片到minio
+
+        String fileName = UUID.randomUUID().toString().replace("-", "");
+        String originalFilename = multipartFile.getOriginalFilename();
+        String postfix = originalFilename.substring(originalFilename.lastIndexOf("."));
+        String fileId = null;
+        try {
+            fileId = fileStorageService.uploadImgFile("", fileName + postfix, multipartFile.getInputStream());
+            log.info("上传图片到MinIO，fileId：{}", fileId);
+        } catch (IOException e) {
+            e.printStackTrace();
+            log.error("WmMaterialServiceImpl 上传文件失败");
+        }
+
+        //把图片url保存到数据库
+        WmMaterial wmMaterial = new WmMaterial();
+        wmMaterial.setUserId(WmThreadLocalUtil.getUser().getId());
+        wmMaterial.setUrl(fileId);
+        wmMaterial.setIsCollection((short)0);
+        wmMaterial.setType((short) 0);
+        wmMaterial.setCreatedTime(new Date());
+        save(wmMaterial);
+        //返回结果
+        return ResponseResult.okResult(wmMaterial);
+    }
+
+    /**
+     * 素材列表查询
+     *
+     * @param dto
+     * @return
+     */
+    @Override
+    public ResponseResult findlist(WmMaterialDto dto) {
+
+        //检查参数
+        dto.checkParam();
+
+        //分页查询
+        IPage page = new Page(dto.getPage(), dto.getSize());
+        LambdaQueryWrapper<WmMaterial> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        //是否收藏
+        if (dto.getIsCollection() != null && dto.getIsCollection() == 1){
+            lambdaQueryWrapper.eq(WmMaterial::getIsCollection, dto.getIsCollection());
+        }
+        //按照用户查询
+        lambdaQueryWrapper.eq(WmMaterial::getUserId, WmThreadLocalUtil.getUser().getId());
+        //时间倒序查询
+        lambdaQueryWrapper.orderByDesc(WmMaterial::getCreatedTime);
+
+        page(page, lambdaQueryWrapper);
+        //结果返回
+        ResponseResult responseResult = new PageResponseResult(dto.getPage(), dto.getSize(), (int)page.getTotal());
+        responseResult.setData(page.getRecords());
+        return responseResult;
+    }
+}
